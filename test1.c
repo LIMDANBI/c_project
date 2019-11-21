3d893ca9b981bc1e95f36ec92b1a0013dbd7db39
SeongBinYoon <yoonseongbin4925@gmail.com>
2019-11-21 19:51:23 +0900

    test1

---
+++ test1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define N 4
+
+int arrsum(int *a){
+	
+	int sum =0;
+	
+	for(int i = 0; i < N; ++i) sum += a[i];
+
+	return sum;
+}
+
+int main(void){
+	int a[N] = {1,2,3,4};
+
+	printf("%d\n", arrsum(a));
+}

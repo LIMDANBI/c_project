04e53a5564ee869a6ab753d2ed18588142a7d1f2
SeongBinYoon <yoonseongbin4925@gmail.com>
2019-11-21 20:25:00 +0900

    added firstnode,secondnode,thirdnode struct

---
+++ calculator.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct fnode{
+	int d1;
+	*p1;
+	*n1;
+	*head;
+
+} fnode;
+
+
+typedef struct snode{
+	int d2;
+	*p2;
+	*n2;
+} snode;
+
+
+typedef struct tnode{
+	int d3;
+	*p3;
+	*n3;
+} tnode;
+
+
+

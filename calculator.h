bf12306332d249123f9cd9544d31e24293f850b2
SeongBinYoon <yoonseongbin4925@gmail.com>
2019-11-22 13:01:44 +0900

    edited prev and next in node

--- calculator.h
+++ calculator.h
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define K 10
+
 typedef struct fnode{
 	int d1;
-	*p1;
-	*n1;
+	struct fnode* p1;
+	struct fnode* n1;
 	*head;
 } fnode;
 
 
 typedef struct snode{
 	int d2;
-	*p2;
-	*n2;
+	struct snode* p2;
+	struct snode* n2;
 } snode;
 
 
 typedef struct tnode{
 	int d3;
-	*p3;
-	*n3;
+	struct tnode* p3;
+	struct tnode* n3;
 } tnode;
 
 typedef struct nodeconnect{
@@ -29,4 +31,6 @@ typedef struct nodeconnect{
 	*p3 = &d2;
 
 
-
+typedef struct numdata(*n) {
+       	a[K] = {0,1,2,3,4,5,6,7,8,9};
+	

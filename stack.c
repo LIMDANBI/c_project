0f27b340ace073d7373ca6cfd52d69d05493fa63
danbibibi <55095806+LIMDANBI@users.noreply.github.com>
2019-12-05 00:41:10 +0900

    Update stack.c

--- stack.c
+++ stack.c
@@ -19,7 +19,7 @@ void push2Ostk(operStk* stk, char c){
 }
 
 char pop4Ostk(operStk* stk){
-  operStkNode* tmp = skt->top;
+  operStkNode* tmp = stk->top;
   char poped = tmp->oper;
   stk->top = tmp->prev;
   free(tmp);

36629272a5fd7c81d76da550f0b0c0464fb9d49a
danbibibi <dlaeksql08222@gmail.com>
2019-12-09 05:43:47 -0500

    ha,,,

---
+++ untilminus/calculator.c
@@ -0,0 +1,226 @@
+# include "cal.h"
+
+NUM* plus(NUM *n1, NUM *n2){
+
+  int carry = 0;
+  NUM *ans = newNUM();
+  numList *intpart = newNumList(); ans->integer = intpart;
+  numList *decimpart = newNumList();ans->decimal = decimpart;
+
+  numNode *d1 = n1->decimal->head;
+  numNode *d2 = n2->decimal->head;
+
+  while(1){   //두수의 소수부분 자리수를 맞춰준다
+    if(d1==NULL && d2==NULL) break;
+    else if(d1==NULL){
+      while(d2!=NULL){
+        appendNum(n1->decimal, 0); d2 = d2->next;
+      }
+      break;
+    }
+    else if(d2==NULL){  //d2== NULL
+      while(d1!=NULL){
+        appendNum(n2->decimal, 0); d1 = d1->next;
+      }
+      break;
+    }
+    else{ //둘다 null X
+      d1 = d1->next; d2 = d2->next;
+    }
+  } //prefect(debugging)
+
+//마이너스 수가 인자로 들어올 수 있음
+  if(n1->sign == 1 && n2->sign == 1)ans->sign = 1;
+  else if(n1->sign == 1){
+    n1->sign = 0; ans = minus(n2, n1);
+    return ans;
+  }
+  else if(n2->sign == 1){
+    n2->sign = 0; ans = minus(n1, n2);
+    return ans;
+  }
+
+  numNode *dtail1 = getNumTail(n1->decimal);
+  numNode *dtail2 = getNumTail(n2->decimal);
+
+  while (dtail1 != NULL) {  //소수 부분 계산 시작
+    int tmp = carry + dtail1->data + dtail2->data;
+    if(tmp>9){
+      carry = tmp/10; tmp = tmp%10;
+      rappendNum(decimpart, tmp);
+    }
+    else{
+      rappendNum(decimpart, tmp);  carry = 0;
+    }
+    dtail1 = dtail1->prev; dtail2 = dtail2->prev;
+  }
+
+  numNode *itail1 = getNumTail(n1->integer);
+  numNode *itail2 = getNumTail(n2->integer);
+
+  while(1){  // 정수부분 계산 시작
+    if(itail1==NULL && itail2==NULL){
+      break;
+    }
+    else if(itail1 == NULL){
+        int tmp = carry + itail2->data;
+        if(tmp>9){
+          carry = tmp/10; tmp = tmp%10;
+          rappendNum(intpart, tmp);
+        }
+        else {
+          rappendNum(intpart, tmp); carry = 0;
+        }
+        itail2 = itail2->prev;
+      }
+      else if(itail2==NULL){ //itail2 == NULL
+        int tmp = carry + itail1->data;
+        if(tmp>9){
+          carry = tmp/10; tmp = tmp%10;
+          rappendNum(intpart, tmp);
+        }
+        else {
+          rappendNum(intpart, tmp); carry = 0;
+        }
+        itail1 = itail1->prev;
+      }
+    else{ //둘다 널이 아닐 때
+      int tmp = carry + itail1->data + itail2->data;
+      if(tmp>9){
+        carry = tmp/10; tmp = tmp%10;
+        rappendNum(intpart, tmp);
+      }
+      else {
+        rappendNum(intpart, tmp); carry = 0;
+      }
+      itail1 = itail1->prev; itail2= itail2->prev;
+    }
+  }
+  if(carry != 0){
+    rappendNum(intpart, carry);
+  }
+  return ans;
+}
+
+// --------------------- "+" ---------------------------------------
+
+NUM* minus(NUM *n1, NUM *n2){
+
+  NUM *ans = newNUM();
+  numList *intpart = newNumList(); ans->integer = intpart;
+  numList *decimpart = newNumList(); ans->decimal = decimpart;
+  int pull = 0;  //정수 부분에서 끌어올 때 사용
+
+  if(n1->sign == 1 && n2->sign == 1) ans->sign = 1;
+  else if(n1->sign == 1){
+    n1->sign = 0; ans = minus(n2, n1);
+    return ans;
+  }
+  else if(n2->sign == 1){
+    n2->sign = 0; ans = minus(n1, n2);
+    return ans;
+  }
+
+  int who = whoMore(n1,n2);  // 누가 더 큰 수 인지 알려 준다
+
+  if(who == 2){ // (빼는 수가 더 큰 경우)작은수에서 큰수를 빼면 결과는 마이너스
+    NUM *tmp = n1; n1 = n2; n2 = tmp;  // 두 값을 반대로 뺀다
+    ans->sign = 1;  // 부호는 마이너스가 될 것
+  }
+  else if(who == 0){ // 두수가 완전히 같다면 그 결과값은 0이 될것임.
+    appendNum(ans->integer, 0);
+    return ans;
+  }
+
+  // who == 1 인 경우 위에서 아무처리도 하지 않고 계산 시작 !!
+  numNode *d1 = n1->decimal->head;
+  numNode *d2 = n2->decimal->head;
+
+  while(1){   //두수의 소수부분 자리수를 맞춰준다. 짧은 수에 빈자리에 0을 채워 줌
+    if(d1==NULL && d2==NULL) break;
+    else if(d1==NULL){
+      while(d2!=NULL){
+        appendNum(n1->decimal, 0); d2 = d2->next;
+      }
+      break;
+    }
+    else if(d2==NULL){  //d2== NULL
+      while(d1!=NULL){
+        appendNum(n2->decimal, 0); d1 = d1->next;
+      }
+      break;
+    }
+    else{ //둘다 null X
+      d1 = d1->next; d2 = d2->next;
+    }
+  }
+
+  numNode *dtail1 = getNumTail(n1->decimal);
+  numNode *dtail2 = getNumTail(n2->decimal);
+
+  while (dtail1 != NULL) { // 소수 부분(같은 자리수를 가지고 있음) 알의 자리 부터 계산 시작
+    if(dtail2->data > dtail1->data){ //앞에서 숫자를 끌어와야 할 때
+      if(dtail1->prev != NULL){
+        dtail1->prev->data--; dtail1->data+=10; //앞에서 수를 끌어옴
+        rappendNum(decimpart, dtail1->data - dtail2->data);
+      }
+      else{
+        pull = 1; dtail1->data+=10; //정수 부분에서 수를 끌어옴
+        rappendNum(decimpart, dtail1->data - dtail2->data);
+      }
+    }
+    else rappendNum(decimpart, dtail1->data - dtail2->data);
+    dtail1 = dtail1->prev; dtail2 = dtail2->prev;
+  }
+
+  numNode *itail1 = getNumTail(n1->integer);
+  numNode *itail2 = getNumTail(n2->integer);
+
+  while (1) {  // 정수 부분 일의 자리 부터 계산 시작
+    if(itail1==NULL && itail2==NULL) break;
+    else if(itail2 == NULL){
+      while(itail1 != NULL){
+        rappendNum(intpart, itail1->data - pull); pull = 0;
+        itail1 = itail1->prev;
+      }
+      break;
+    }
+    else{  //둘다 널이 아닐 때
+      if(itail1->data>=itail2->data){
+        rappendNum(intpart, itail1->data-itail2->data - pull); pull = 0;
+      }
+      else{  // 빼는 값이 같거나 더 큰 경우
+        itail1->prev->data--; itail1->data+=10;
+        rappendNum(intpart, itail1->data-itail2->data - pull); pull = 0;
+      }
+      itail1 = itail1->prev; itail2 = itail2->prev;
+    }
+  }
+
+  return ans;
+}
+
+//----------------------- "-" --------------------------------------
+
+NUM* multi(NUM *n1, NUM *n2){
+  NUM *ans = newNUM();
+  numList *intpart = newNumList(); ans->integer = intpart;
+  numList *decimpart = newNumList(); ans->decimal = decimpart;
+  if(n1->sign != n2->sign) ans->sign = 0;  // 둘중 하나가 음수면 답은 음수, 아니면 양수
+  else ans->sign = 1;
+
+  while (1) {
+
+  }
+
+
+}
+
+//----------------------- "*"----------------------------------------
+
+// NUM* divi(NUM *n1, NUM *n2){
+//
+// }
+
+// -------------- the end (until divi) -------------------------------
+
---
+++ untilminus/numList.c
@@ -0,0 +1,124 @@
+# include "cal.h"
+
+numNode* newNumNode(int data){  // [prev - data - next] 형의 노드
+  numNode* tmp = (numNode*)malloc(sizeof(numNode));
+  tmp->data = data;
+  tmp->next = NULL;
+  tmp->prev = NULL;
+  return tmp;
+}
+
+numList* newNumList(){  // 정수, 소수 부분을 리스트로 연결
+  numList* tmp = (numList*)malloc(sizeof(numList));
+  tmp->head = NULL;
+  return tmp;
+}
+
+NUM* newNUM(){  // 부호+정수부분+소수부분 (즉, 하나의 숫자)
+  NUM* tmp = (NUM*)malloc(sizeof(NUM));
+  tmp->sign = 0;
+  tmp->decimal = NULL;
+  tmp->integer = NULL;
+  return tmp;
+}
+
+numNode* getNumTail(numList* list){  //원하는 리스트의 꼬리노드를 알 수 있음
+  numNode* tmp = list->head;
+  if(tmp == NULL){
+    return tmp;
+  }
+  while(tmp->next != NULL){
+    tmp = tmp->next;
+  }
+  return tmp;
+}
+
+void appendNum(numList* list, int data){  //원하는 리스트에 data를 추가할 수 있음
+  numNode* tmp = newNumNode(data);
+  if(list->head == NULL){
+    list->head = tmp;
+  }
+  else{
+    numNode* tail = getNumTail(list);
+    tail->next = tmp;
+    tmp->prev = tail;
+  }
+}
+
+void rappendNum(numList *list, int data){  //appendNum과 반대로 거꾸로 데이터 추가
+  numNode *tmp = newNumNode(data);
+  if(list->head == NULL){
+    list->head = tmp;
+  }
+  else{
+    numNode *recent = list->head;
+    recent->prev = tmp;
+    tmp->next = recent;
+    list->head = tmp;
+  }
+}
+
+void printNum(NUM *n){  //NUM을 프린트 해주는 함수
+  numNode *tmp = n->integer->head;
+  if(n->sign) printf("-");
+  while(tmp!=NULL){
+    printf("%d",tmp->data);
+    tmp = tmp->next;
+  }
+  if(n->decimal->head != NULL) {
+    printf(".");
+    tmp = n->decimal->head;
+    while(tmp!=NULL) {
+      printf("%d",tmp->data);
+      tmp = tmp->next;
+    }
+  }
+}
+
+int whoMore(NUM *n1, NUM *n2){  // 두 넘을 받아서 누가 더 큰지 리턴해주는 함수
+                                // 0 :같다 , 1: n1이 더 크다 , 2: n2가 더 크다.
+  numNode *intpart1 = n1->integer->head;
+  numNode *intpart2 = n2->integer->head;
+  while (1) {  // 정수 부분 부터 체크
+    if(intpart1==NULL && intpart2==NULL){  // 길이가 같을 때
+      intpart1 = n1->integer->head; intpart2 = n2->integer->head;
+      while (intpart1!=NULL) {
+        if(intpart1->data > intpart2->data) return 1;
+        else if(intpart2->data > intpart1->data) return 2;
+        intpart1 = intpart1->next; intpart2 = intpart2->next;
+      }
+      break;
+    }
+    else if(intpart1==NULL) return 2;  // 만약 1.11000 과 1.11이라면,,
+    else if(intpart2==NULL) return 1;
+    intpart1 = intpart1->next; intpart2 = intpart2->next;
+  }
+
+  numNode *decimpart1 = n1->decimal->head;
+  numNode *decimpart2 = n2->decimal->head;
+  while (decimpart1!=NULL && decimpart2!=NULL) { // 소수 부분 체크
+    if(decimpart1->data>decimpart2->data) return 1;
+    else if(decimpart2->data>decimpart1->data) return 2;
+    decimpart1 = decimpart1->next; decimpart2 = decimpart2->next;
+  }
+  if(decimpart1 != NULL) return 1;  // 다음 수가 0으로 끝난다면??
+  else if(decimpart2 != NULL) return 2;
+  return 0;
+}
+
+void deletzero(NUM *n){    // 불필요한 앞뒤 0 을 제거해주는 함수
+  numNode *intpart = n->integer->head;
+  numNode *decimpart = getNumTail(n->decimal);
+  while(intpart->data == 0){
+    numNode *freed = intpart;
+    intpart = intpart->next;
+    n->integer->head = intpart;
+    // free(freed);
+  }
+  while(decimpart->data == 0){
+    numNode *freed = decimpart;
+    decimpart = decimpart->prev;
+    // free(freed);
+  }
+}
+

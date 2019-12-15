00799763637d7beef82a8c01a1e0c09680be9026
danbibibi <dlaeksql08222@gmail.com>
2019-12-15 14:15:37 -0500

    now-maintest

--- main.c
+++ main.c
@@ -76,12 +76,14 @@ int main(){
           if(store == '+' || store == '-' || store == '*' ||store == '('){
             printf(" !! Wrong expression. !! \n"); exit(1);
           }
-          else if(store == ')') appendExpression(infix, newExpressionNode(NULL, c));
           else{
-            appendExpression(infix, newExpressionNode(nNum, 0));
-            checkPoint = 0; nNum = newNUM(); oLap = 0;
-            intpart = newNumList(); decimpart = newNumList();
-            nNum->integer = intpart; nNum->decimal = decimpart;
+            if(nNum->integer != NULL){
+              appendExpression(infix, newExpressionNode(nNum, 0));
+              checkPoint = 0; nNum = newNUM(); oLap = 0;
+              intpart = newNumList(); decimpart = newNumList();
+              nNum->integer = intpart; nNum->decimal = decimpart;
+            }
+            appendExpression(infix, newExpressionNode(NULL, c));
           }
         }
       }
@@ -101,109 +103,48 @@ int main(){
       if(store != ' ') store = c;
     }
 
-
     appendExpression(infix, newExpressionNode(nNum, 0));
+    
+    expressionNode *in1 = infix->head;
+    while (in1 != NULL) {
+      if(operCheck(in1->oper)) {
+        printf("%c", in1->oper);
+      }
+      else{
+        printNum(in1->num);
+      }
+      in1 = in1->next;
+    }
+    printf("\n");
+
+
+    expressionNode *in = infix->head;
     expressionList* postfix = newExpressionList();
     in2post(infix, postfix);
 
-    expressionNode *path = postfix->head;
-    expressionNode *freed;
-    numStk *nStk = newNumStk();  //계산시 숫자를 보관하고 빼서 쓸 스택 선언
-
-    while(path!=NULL){   //postfix식을 돌면서 계산을 함
-
-      if(operCheck(path->oper)){ //연산자인 경우
-
-        if(path->oper == '+'){  //스택에서 두 수를 빼서 계산하고 결과 값을 다시 스택에 넣는다.
-          NUM *process2 = pop4Nstk(nStk); NUM *process1 = pop4Nstk(nStk);
-          NUM *result; int who = whoMore(process1, process2);
-          if(process1->sign == process2->sign ){  // 두 수의 부호가 같을 때
-            result = plus(process1, process2);
-            if(process1->sign == 1) result->sign = 1;
-          }
-
-          else{  // process1->sign != process2->sign
-            if(process1->sign == 1){  //process2->sign = 0;
-              if(who == 1) {  // -9+6
-                result = minus(process1, process2); result->sign = 1;
-              }
-              else if(who == 0 || who == 2){
-                result = minus(process2, process1);
-              }
-            }
-            else{  // process1->sign = 0; process2->sign = 1;
-              if(who == 0 || who == 1) result = minus(process1, process2);
-              else if(who == 2){  //8+-9
-                process2->sign = 0;
-                result = minus(process2, process1); result->sign = 1;
-              }
-            }
-          }
-          push2Nstk(nStk, result);
-        }
-
-        else if(path->oper == '-'){
-          NUM *process2 = pop4Nstk(nStk); NUM *process1 = pop4Nstk(nStk);
-          NUM *result; int who = whoMore(process1, process2);
-          if(process1->sign == process2->sign ){  // 두 수의 부호가 같을 때
-            if(process1->sign == 0){  //둘다 양수
-              if(who == 0 || who == 1){
-                result = minus(process1, process2);
-              }
-              else if(who == 2){
-                result = minus(process1, process2); result->sign = 1;
-              }
-            }
-            else{ // 둘다 음수
-              if(who == 1){
-                result = minus(process1, process2); result->sign = 1;
-              }
-              else if(who == 0 || who == 2){
-                result = minus(process2, process1); result->sign = 0;
-              }
-            }
-          }
-
-          else{  // process1->sign != process2->sign
-            if(process1->sign == 1){  //process2->sign = 0;
-              result = plus(process1, process2); result->sign = 1;
-            }
-            else{  // process1->sign = 0; process2->sign = 1;
-              result = plus(process1, process2);
-            }
-          }
-          push2Nstk(nStk, result);
-        }
-
-        else if(path->oper == '*'){
-          NUM *process2 = pop4Nstk(nStk); NUM *process1 = pop4Nstk(nStk);
-          NUM *result = multi(process1, process2);;
-          if(process1->sign != process2->sign) result->sign = 1;
-          push2Nstk(nStk, result);
-        }
-
+    while (in != NULL) {
+      if(operCheck(in->oper)) {
+        printf("%c", in->oper);
       }
-      else{ // 넘인 경우
-        push2Nstk(nStk, path->num);
+      else{
+        printNum(in->num);
       }
-      freed = path; path = path->next;
-      free(freed);
+      in = in->next;
     }
+    printf("\n");
 
-    NUM *final_ans = pop4Nstk(nStk); deletzero(final_ans);
-    printNum(final_ans); printf("\n");
+    // expressionList* postfix = newExpressionList();
+    // in2post(infix, postfix);
 
     expressionNode *path1 = postfix->head;
-    // while (path1!=NULL) {
-    //   if(operCheck(path1->oper)) {
-    //     printf(" %c ", path1->oper);
-    //   }
-    //   else{
-    //     printf(" "); printNum(path1->num); printf(" ");
-    //   }
-    //   path1 = path1->next;
-    // }
-    if(path1 == NULL) printf("freesuscces\n");
-    else printf("Nofreesuscces\n");
+    while (path1!=NULL) {
+      if(operCheck(path1->oper)) {
+        printf(" %c ", path1->oper);
+      }
+      else{
+        printf(" "); printNum(path1->num); printf(" ");
+      }
+      path1 = path1->next;
+    }
   }
 
